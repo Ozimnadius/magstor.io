f7eb3871d0bed3f96d1fb174f8dfab9ed56ea098
Mikle Seregin <mikhail-seregin@yandex.ru>
2020-04-01 18:44:16 +0300

    add readme.md

--- js/pages/catalog.js
+++ js/pages/catalog.js
@@ -2,12 +2,7 @@
 $(function () {
 
     // VARIABLES
-    const rangeFrom = $('.range__input_from'),
-        rangeTo = $('.range__input_to'),
-        range = $('.range__slider'),
-        rangeMin = range.data('min'),
-        rangeMax = range.data('max'),
-        form = $('.filters');
+    const form = $('.filters');
 
     // EVENTS
     //Раскрытие фильтров
@@ -39,81 +34,6 @@ $(function () {
         }
     });
 
-    //ввод цифр в поля
-    $('.range__input').on('keypress', function (e) {
-        if (e.key.match(/[^0-9]/g) || (this.value == 0 && e.key == 0)) {
-            e.preventDefault();
-        }
-    });
-
-    //Изменение данных
-    $('.range__input').on('input', function (e) {
-        let $this = $(this),
-            val = '',
-            from = getNumber(rangeFrom.val()),
-            to = getNumber(rangeTo.val());
-
-        if ($this.hasClass('range__input_from')) {
-            if (from < rangeMin) {
-                from = rangeMin;
-            } else if (from > to) {
-                from = to;
-            }
-            val = from;
-        } else {
-            if (to > rangeMax) {
-                to = rangeMax;
-            } else if (to < from) {
-                to = from;
-            }
-            val = to;
-        }
-
-        let valPretty = prettify(val);
-
-        rangeSlider.update({
-            from: from,
-            to: to
-        });
-
-        $this.val(valPretty);
-    });
-
-    //Изменение чеков
-    $('.check__input').on('change', function (e) {
-        let $this = $(this),
-            filter = $this.closest('.filter');
-
-        let checked = checkFilter(filter);
-
-        if (checked) {
-            filter.addClass('filled');
-        } else {
-            filter.removeClass('filled');
-        }
-
-    });
-
-    //Сброс фильтра
-    $('.filter__clear').on('click', function (e) {
-        e.preventDefault();
-        let $this = $(this),
-            filter = $this.closest('.filter');
-
-        clearFilter(filter);
-    });
-
-    //Сброс фильтров
-    $('.filters__reset').on('click', function (e) {
-        let btn = $(this),
-            form = btn.closest('.filters');
-        form.trigger('reset');
-        rangeUpdate();
-
-        $('.filter').each(function (x, i) {
-            clearFilter($(i));
-        });
-    });
 
     //Открытие формы фильтров
     $('.jsFiltersOpen').on('click', function (e) {
@@ -126,38 +46,6 @@ $(function () {
     });
 
     // FUNCTIONS
-    function rangeUpdate() {
-        let from = getNumber(rangeFrom.val()),
-            to = getNumber(rangeTo.val());
-
-        rangeSlider.update({
-            from: from,
-            to: to
-        });
-    }
-
-    function checkFilter(filter) {
-        let checks = filter.find('.check__input'),
-            checked = false;
-
-        checks.each(function (x, i) {
-            if (i.checked) {
-                checked = true;
-            }
-        });
-
-        return checked;
-    }
-
-    function clearFilter(filter) {
-        let checks = filter.find('.check__input');
-
-        checks.each(function (x, i) {
-            i.checked = false;
-        });
-        filter.removeClass('filled');
-    }
-
     function closeFilters() {
         let filters = $('.filter'),
             filtersBottom = $('.filter__bottom');
@@ -177,21 +65,6 @@ $(function () {
 
 
     //VENDORS
-    $(".range__slider").ionRangeSlider({
-        prettify_enabled: true,
-        skin: 'round',
-        hide_from_to: true,
-        // hide_min_max: true,
-        onChange: function (data) {
-            rangeFrom.val(data.from_pretty);
-            rangeTo.val(data.to_pretty);
-        },
-        onFinish: function (data) {
-            //ToDo Здесь должень быть ajax на получение товаров
-        },
-    });
-    let rangeSlider = $(".range__slider").data("ionRangeSlider");
-
     //Если планшет или мобила, то клик отрабатывает как стандартный ховер
     if (device.tablet() || device.mobile()) {
         $('.jsHoverLink').on('click', function (e) {
